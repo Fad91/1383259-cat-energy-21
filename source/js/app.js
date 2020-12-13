48e3f1a877ccab43d80d044cd594ae38e54e59a0
Fad91 <homyak91@inbox.ru>
2020-12-13 18:02:22 +0400

    update

--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,9 @@
 let menuButton = document.querySelector(".nav__button");
 let mainMenu = document.querySelector(".main-menu");
+let beforeButton = document.querySelector(".slider__button-before");
+let afterButton = document.querySelector(".slider__button-after");
+let beforeSlide = document.querySelector(".slider__image-before");
+let afterSlide = document.querySelector(".slider__image-after");
 
 mainMenu.classList.add("main-menu--closed");
 
@@ -9,5 +13,17 @@ menuButton.addEventListener("click", function (evt) {
 });
 
 if(menuButton.classList.contains("nav__button--closed")) {
-  mainMenu.classList.remove("main-menu--closed")
+  mainMenu.classList.remove("main-menu--closed");
 };
+
+afterButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  afterSlide.classList.remove("slider__image-after--closed");
+  beforeSlide.classList.add("slider__image-before--closed");
+});
+
+beforeButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  beforeSlide.classList.remove("slider__image-before--closed");
+  afterSlide.classList.add("slider__image-after--closed");
+});
